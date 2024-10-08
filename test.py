8a51695530ad2da277464431fa489a2adedf92dc
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 04:31:10 -0300

    defining what need to be done. to be continued

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
-
 from secure_pass4.crypt_pass4.crypto_pass import criptografar_senha, descriptografar_senha
+from secure_pass4.save_pass4.save_pass import salvar_senha, carregar_senhas
+from secure_pass4.secure_pass4 import gerar_senha, avaliar_forca
 
 class TestSecurePass4(unittest.TestCase):
     def setUp(self):
@@ -13,22 +14,27 @@ class TestSecurePass4(unittest.TestCase):
     def test_criptografar_senha(self):
         """Teste para verificar a criptografia da senha."""
         resultado = criptografar_senha(self.senha)
+        #print(resultado)
         self.assertNotEqual(resultado, self.senha, "A senha criptografada deve ser diferente da senha original.")
 
         resultado = descriptografar_senha(resultado)
+        #print(resultado)
         self.assertEqual(resultado, self.senha, "A senha criptografada deve ser igual aa senha original.")
         
 
     def test_descriptografar_senha(self):
+        """Teste para verificar a descriptografia da senha."""
         # test strong passWW
         resultado = descriptografar_senha(self.senha_criptografada)
+        #print(resultado)
         self.assertEqual(resultado, self.senha, "A senha criptografada deve ser igual a senha original.")
         
         # test week pass
         resultado = descriptografar_senha(self.senha_fraca_cripto)
+        #print(resultado)
         self.assertEqual(resultado, self.senha_fraca, "A senha criptografada deve ser igual a senha original.")
-        
-        
+
+
 
 if __name__ == "__main__":
     unittest.main()
