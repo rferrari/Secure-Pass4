b28f1a2f0ccc63f3311e665ec5ae8e2d96febb3f
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 03:41:25 -0300

    define: create and load key

--- secure_pass4/crypt_pass4.py
+++ secure_pass4/crypt_pass4.py
@@ -1,3 +1,9 @@
+def gerar_chave():
+    """Gera e salva uma chave para criptografia/descriptografia."""
+
+def carregar_chave():
+    """Carrega a chave usada para criptografar/descriptografar as senhas."""
+
 def criptografar_senha(senha):
     """Criptografa uma senha."""
     senha_criptografada = ""
