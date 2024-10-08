68903aafbfa91c7c45f8ea75a1482d2e50542710
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 04:55:53 -0300

    feat: crypto_pass salvar_senha to file

--- secure_pass4/save_pass4/save_pass.py
+++ secure_pass4/save_pass4/save_pass.py
@@ -1,5 +1,12 @@
+import os
+from secure_pass4.crypt_pass4.crypto_pass import criptografar_senha, descriptografar_senha
+
 def salvar_senha(site, senha):
     """Salva uma senha criptografada para um site em um arquivo de texto."""
+    senha_criptografada = criptografar_senha(senha)
+
+    with open("securepass4.safe", "a") as arquivo_senhas:
+        arquivo_senhas.write(f"{site}: {senha_criptografada.decode()}\n")
 
 def carregar_senhas():
     """Carrega e descriptografa todas as senhas do arquivo de senhas."""
