2a16f7835a12658e80fb261e80b61c03f93b760a
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 04:23:00 -0300

    add: save load pass file

---
+++ secure_pass4/save_pass4/save_pass.py
@@ -0,0 +1,5 @@
+def salvar_senha(site, senha):
+    """Salva uma senha criptografada para um site em um arquivo de texto."""
+
+def carregar_senhas():
+    """Carrega e descriptografa todas as senhas do arquivo de senhas."""
