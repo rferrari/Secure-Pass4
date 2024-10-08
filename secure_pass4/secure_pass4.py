8e83da77db5733ab0cd6da5e9893d07a2e26b1cf
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 04:21:51 -0300

    add: main app, create and analyse passwords

---
+++ secure_pass4/secure_pass4.py
@@ -0,0 +1,5 @@
+def gerar_senha(comprimento=12, incluir_letras=True, incluir_numeros=True, incluir_simbolos=True):
+    """Gera uma senha aleatória de acordo com as preferências do usuário."""
+
+def avaliar_forca(senha):
+    """Avalia a força da senha com base em seu comprimento e complexidade."""
