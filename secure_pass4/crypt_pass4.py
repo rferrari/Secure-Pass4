d44ea188029ee89b207d9153f6badbcff6352ca5
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-08 03:39:59 -0300

    def: criptografar_descriptografar senhas

---
+++ secure_pass4/crypt_pass4.py
@@ -0,0 +1,9 @@
+def criptografar_senha(senha):
+    """Criptografa uma senha."""
+    senha_criptografada = ""
+    return senha_criptografada
+
+def descriptografar_senha(senha_criptografada):
+    """Descriptografa uma senha."""
+    senha = ""
+    return senha
